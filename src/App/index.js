d633bb99d7fc1fd6dbb19ac8d000b342e5efd964
Casey Gillan <gillan.casey@gmail.com>
2018-10-16 11:58:12 -0400

    Added browser router and provatebrowser component

--- src/App/index.js
+++ src/App/index.js
@@ -1,19 +1,23 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
 import "./style.css";
 import ActivityPage from "../ActivityPage";
 import Register from "../Register";
 import Login from "../Login";
 import Profile from "../Profile";
+import PrivateRoute from "../PrivateRoute";
 
 class App extends Component {
   render() {
     return (
-    <div className="App">
-      <Register />
-      <Login />
-      <ActivityPage />
-      <Profile />
-    </div>
+      <Router>
+        <div className="App">
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/" component={Login} />
+          <Route exact path="/activity" component={ActivityPage} />
+          <Route exact path="/profile" component={Profile} />
+        </div>
+      </Router>
     )
   }
 }
